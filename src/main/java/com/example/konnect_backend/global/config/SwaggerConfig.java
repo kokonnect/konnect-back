d2396a84384cb39282083f437cb9e2f5a23fce46
haeun <haeun9634@naver.com>
2025-12-04 10:22:14 +0900

    fix: Swagger 서버 URL 직접 명시하여 체크하도록 수정

--- src/main/java/com/example/konnect_backend/global/config/SwaggerConfig.java
+++ src/main/java/com/example/konnect_backend/global/config/SwaggerConfig.java
@@ -3,10 +3,13 @@ package com.example.konnect_backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,17 +17,30 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String jwt = "JWT";
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
+        Components components = new Components().addSecuritySchemes(jwt,
+                new SecurityScheme()
+                        .name(jwt)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
         );
+
+        // 서버 URL 명시 (http 대신 https)
+        Server productionServer = new Server()
+                .url("https://api.konnect-women.site")
+                .description("Production Server (HTTPS)");
+
+        Server localServer = new Server()
+                .url("http://localhost:8080")
+                .description("Local Development Server");
 
         return new OpenAPI()
                 .components(components)
                 .info(apiInfo())
+                .addServersItem(productionServer)
+                .addServersItem(localServer)
                 .addSecurityItem(securityRequirement);
     }
 
