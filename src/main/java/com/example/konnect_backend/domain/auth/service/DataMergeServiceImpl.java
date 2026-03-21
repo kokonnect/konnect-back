e47839d6099efcdc93d89c5569e8ed67287a634b
haeun <haeun9634@naver.com>
2026-03-21 23:05:08 +0900

    fix: 빌드 에러 수정

--- src/main/java/com/example/konnect_backend/domain/auth/service/DataMergeServiceImpl.java
+++ src/main/java/com/example/konnect_backend/domain/auth/service/DataMergeServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.konnect_backend.domain.user.entity.Device;
 import com.example.konnect_backend.domain.user.entity.User;
 import com.example.konnect_backend.domain.user.repository.DeviceRepository;
 import com.example.konnect_backend.domain.user.repository.UserRepository;
+import com.example.konnect_backend.global.code.status.ErrorStatus;
+import com.example.konnect_backend.global.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
