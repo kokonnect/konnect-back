ac8b139db1fcdcc3294e8f462a72a211a40669ee
haeun <haeun9634@naver.com>
2026-03-22 23:19:41 +0900

    fix: Device 저장시 타겟 언어 함께 저장

--- src/main/java/com/example/konnect_backend/domain/user/controller/DeviceController.java
+++ src/main/java/com/example/konnect_backend/domain/user/controller/DeviceController.java
@@ -1,11 +1,9 @@
 package com.example.konnect_backend.domain.user.controller;
 
+import com.example.konnect_backend.domain.user.entity.status.Language;
 import com.example.konnect_backend.domain.user.service.DeviceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +13,10 @@ public class DeviceController {
     private final DeviceService deviceService;
 
     @PostMapping("/register")
-    public void register(@RequestHeader("X-Device-Id") String deviceUuid) {
-        deviceService.registerDevice(deviceUuid);
+    public void register(
+            @RequestHeader("X-Device-Id") String deviceUuid,
+            @RequestParam(value = "targetLanguage", required = false)Language language
+            ) {
+        deviceService.registerDevice(deviceUuid, language);
     }
 }
